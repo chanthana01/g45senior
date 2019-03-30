12e440eb6314831497d73fb61f390e2aa505989f
chanthana bunchongpru <Not>
2019-03-30 15:49:02 +0700

    add norm

--- routes/norm.js
+++ routes/norm.js
@@ -26,7 +26,33 @@ router.post("/calculatenorm", function(req, res, next) {
   if(req.session.status === undefined){
     return res.sendStatus(401)
   }
-  // console.log(req.body);
+  console.log(req.body);
+  let norm = req.body.norm;
+  let solution = "";
+  let solution2 = "";
+  let solution3 = 0;
+  let answer =0;
+  for(let i = 0;i<norm.length;i++){
+    solution += "("+norm[i]+"^2"+")+";
+    solution2 += math.pow(parseFloat(norm[i]),2)+"+";
+    solution3 = solution3 + math.pow(parseFloat(norm[i]),2);
+    answer= answer + math.pow(parseFloat(norm[i]),2);
+  }
+  solution=solution.substring(0, solution.length - 1);
+  solution2=solution2.substring(0, solution2.length - 1);
+  answer = math.sqrt(answer);
+  console.log(solution);
+  console.log(solution2);
+  console.log(solution3);
+  console.log(answer);
+
+  res.json({
+    solution: solution,
+    solution2: solution2,
+    solution3: solution3,
+    answer:answer
+  });
+
   });
 
 
